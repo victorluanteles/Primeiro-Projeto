9bc9fa9d7e0edbf090662705ef0fdedf16f043db
Victor Luan <victor_luan12@hotmail.com>
2019-05-28 18:46:58 -0300

    correção para abertura de template

--- APLICACAO/urls.py
+++ APLICACAO/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
-app_name="pokedex"
+
+app_name = "pokedex"
+
 urlpatterns = [
     path("listar", listarPokemons, name="listar_pokemons"),
     path("pokemon/<int:idpokemon>/", umPokemon, name="um_pokemon")
